26519af0fbda1d58ac93bd33b11e6471ab9a9b1e
Jiadi Su <20302010043@fudan.edu.cn>
2022-05-27 02:09:54 +0800

    完成购物车界面美化

--- WebContent/cart.php
+++ WebContent/cart.php
@@ -6,72 +6,142 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./static/css/global.css">
+    <link rel="stylesheet" href="./static/css/navigation.css">
     <link rel="stylesheet" href="./static/css/cart.css">
-    <title>ShoppingCart</title>
+    <title>购物车</title>
 </head>
 
 <body>
 <!-- 导航栏 -->
 <nav>
     <div class="top">
-        <ul>
-            <li><a href="profile.php">个人中心</a></li>
-            <li><a href="cart.php">购物车</a></li>
-            <li><a href="#">登出</a></li>
-        </ul>
+        <!-- 左侧导航栏 -->
+        <div class="left">
+            <!-- welcome -->
+            <div class="welcome">欢迎来到</div>
+            <!-- 艺术字设置为背景图片 -->
+            <div class="name">
+                <a href="#">ArtStore</a>
+            </div>
+            <div class="welcome"> welcome to art store </div>
+        </div>
+        <!-- 右侧选项栏，由php动态生成 -->
     </div>
 </nav>
-<!-- logo与搜索栏 -->
-<div class="headers">
+<!-- 头部栏 -->
+<div class="header w">
+    <!-- logo部分 -->
     <div class="logo">
-        <h1>Art Trading</h1>
+        <img src="./static/img/logo.png" alt="logo">
     </div>
-    <!-- 搜索框 -->
-    <div class="container">
-        <input type="text" class="search" placeholder="搜索">
+    <!-- 导航栏部分 -->
+    <div class="nav">
+        <ul>
+            <li><a href="index.php">首页</a></li>
+            <li><a href="profile.php">个人中心</a></li>
+            <li><a href="upload.php">发布艺术品</a></li>
+        </ul>
+    </div>
+    <!-- 搜索部分 -->
+    <div class="search">
+        <input type="text" placeholder="请输入关键词" id="search-input">
+        <button id="search-btn"></button>
     </div>
 </div>
-<!-- 操作导航栏 -->
-<div class="opeartion-navbar">
-    <div class="opeartion">首页</div>
-    <div class="opeartion">搜索</div>
-    <div class="opeartion">详情</div>
-    <div class="opeartion">发布艺术品</div>
-</div>
-<!-- 购物车 -->
-<h2 style="margin-left: 30px;">购物车</h2>
-<div class="cart">
-    <div class="cart-item">
-        <img src="./static//img/rotation6.webp" alt="">
-        <div class="item-introduction">
-            <div class="item-resume">
-                介绍：XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+<!-- 核心内容 -->
+<div class="box">
+    <div class="cart w">
+        <!-- 购物车基本信息 -->
+        <div class="cart-head">
+            <h3> 用户a 的购物车（全部 100 个）</h3>
+            <div class="selected">
+                已选商品（不含运费） <span>0.00</span>
+                <button> 结算 </button>
             </div>
-            <div class="item-resume">
-                价格：￥99999
+        </div>
+        <div class="all-check">
+            <input type="checkbox"> 全选
+        </div>
+        <!-- 购物车的内容 -->
+        <div class="cart-item">
+            <div class="check">
+                <input type="checkbox">
             </div>
-            <div class="item-operation">
-                <button style="background-color: aqua;">购买</button>
-                <button style="background-color: red;">删除</button>
+            <div class="image">
+                <img src="./static/img/works/medium/006040.jpg">
+            </div>
+            <div class="art-info">
+                <h3> Art Name • Author Name</h3>
+                <div style="margin-top: 5px"> Description </div>
+            </div>
+            <div class="tips">
+                该艺术品信息存在变动<br>
+                <span class="update">点击更新</span>
+            </div>
+            <div class="price"> $999.000 </div>
+            <div class="operation">
+                <span>从购物车中删除</span>
+            </div>
+        </div>
+        <div class="cart-item">
+            <div class="check">
+                <input type="checkbox">
+            </div>
+            <div class="image">
+                <img src="./static/img/works/medium/006040.jpg">
+            </div>
+            <div class="art-info">
+                <h3> Art Name • Author Name</h3>
+                <div style="margin-top: 5px"> Description </div>
+            </div>
+            <div class="tips">
+                该艺术品信息存在变动<br>
+                <span class="update">点击更新</span>
+            </div>
+            <div class="price"> $999.000 </div>
+            <div class="operation">
+                <span>从购物车中删除</span>
             </div>
         </div>
     </div>
-    <div class="cart-item">
-        <img src="./static//img/rotation3.webp" alt="">
-        <div class="item-introduction">
-            <div class="item-resume">
-                介绍：XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-            </div>
-            <div class="item-resume">
-                价格：￥99999
-            </div>
-            <div class="item-operation">
-                <button class="buy-btn">购买</button>
-                <button class="delete-btn">删除</button>
-            </div>
+</div>
+<!-- 底部 -->
+<div class="footer">
+    <div class="w">
+        <div class="copyright">
+            <img src="./static/img/logo-full.png" alt="">
+            <p>
+                艺术品交易网站……
+            </p>
+        </div>
+        <div class="links">
+            <dl>
+                <dt>关于ArtTrading</dt>
+                <dd>关于</dd>
+                <dd>团队管理</dd>
+                <dd>工作机会</dd>
+                <dd>客户服务</dd>
+                <dd>帮助</dd>
+            </dl>
+            <dl>
+                <dt>关于ArtTrading</dt>
+                <dd>关于</dd>
+                <dd>团队管理</dd>
+                <dd>工作机会</dd>
+                <dd>客户服务</dd>
+                <dd>帮助</dd>
+            </dl>
+            <dl>
+                <dt>关于ArtTrading</dt>
+                <dd>关于</dd>
+                <dd>团队管理</dd>
+                <dd>工作机会</dd>
+                <dd>客户服务</dd>
+                <dd>帮助</dd>
+            </dl>
         </div>
     </div>
-    <button class="apply-btn">结账</button>
 </div>
 </body>
 
